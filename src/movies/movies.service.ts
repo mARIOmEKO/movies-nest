c1dff81b69d9b85e51fdb3e51b3fd508a9bd97b2
Mario Meko <marioo.meko@outlook.com>
2022-11-07 18:09:36 +0100

    remove user from watchlist if u watch the movie

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -26,30 +26,27 @@ export class MoviesService {
     }
 
     async getMoviesWishlistedByUser(user:User){
+        
+
         return await this.moviesRepository.findBy({userIdWishlisted: user.id.toString()})
     }
 
     async watchMovie(user:User, id:number){
-        const stringId = user.id.toString();
         const movie = await this.moviesRepository.findOne({where: {id: id}})
         if(!movie)
             throw new NotFoundException('Movie not found')
    
         if(movie.userIdWatched.includes(user.id.toString()))
             throw new ConflictException('U have already watched this movie')
- //PER TU PARE !!!!
-        // if(movie.userIdWishlisted.includes(user.id.toString())){
-        //     movie.userIdWishlisted.filter(movie =>( movie = stringId))
-        //     console.log(stringId)
-        //     console.log(movie.userIdWishlisted)
 
-        //     movie.wishlistedBy.filter(user => ( user != user))
-        //     console.log('movie removed from watchlist, added to watched')
-        //     console.log(movie.wishlistedBy)
-        // }
+        if(movie.userIdWishlisted.includes(user.id.toString())){
+            this.removeUserIdWatchlisted(movie.userIdWishlisted, user.id.toString())
+            this.removeUser(movie.wishlistedBy, 'id' , user.id )
+            console.log('movie removed from watchlist, added to watched')
+        }
         movie.userIdWatched.push(user.id.toString())
         movie.watchedBy.push(user)
-        // await this.moviesRepository.save(movie)
+        await this.moviesRepository.save(movie)
         return movie;
     }
 
@@ -71,5 +68,27 @@ export class MoviesService {
     }
 
     
+      removeUser (arr, attr, value){
+        var i = arr.length;
+        while(i--){
+           if( arr[i] 
+               && arr[i].hasOwnProperty(attr) 
+               && (arguments.length > 2 && arr[i][attr] === value ) ){ 
+    
+               arr.splice(i,1);
+    
+           }
+        }
+        return 'success';
+    }
+
+    removeUserIdWatchlisted(arr, value){
+        for( var i = 0; i < arr.length; i++){ 
+    
+            if ( arr[i] === value) { 
+        
+                arr.splice(i, 1); 
+            }}
+    }
 
 }
