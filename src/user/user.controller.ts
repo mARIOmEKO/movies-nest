96b038ee566ab03d4570383b3c01891d99d0cd18
Mario Meko <marioo.meko@outlook.com>
2022-12-06 19:34:50 +0100

    admin now can see all user and delete user if desired

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import RoleGuard from 'src/auth/role.guard';
+import { DeleteResult } from 'typeorm';
+import { Role } from './role.enum';
+import { UserService } from './user.service';
+import { User } from './users.entity';
 
+
+@UseGuards(RoleGuard(Role.Admin))
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService,
+    ){}
+
+    @Get()
+    getAllUsers():Promise<User[] | undefined>{
+        return this.userService.getAllUsers()
+    }
+
+    @Get(':id')
+    getUserById(
+        @Param('id', ParseIntPipe) id:number
+    ): Promise<User | undefined>{
+        return this.userService.getUserById(id)
+    }
+
+    @Delete(':id')
+    deleteAccount(
+        @Param('id', ParseIntPipe) id:number
+    ):Promise<DeleteResult>{
+        return this.userService.deleteAccount(id)
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
+
+    async getAllUsers() : Promise<User[] | undefined>{
+        return await this.userRepository.find()
+    }
+
+    async getUserById(id: number) : Promise<User | undefined>{
+        return await this.userRepository.findOne({where: {id}})
+    }
+
+    async deleteAccount(id: number) : Promise<DeleteResult>{
+        return await this.userRepository
+        .createQueryBuilder('u')
+        .delete()
+        .where('id = :id', {id})
+        .execute()
+    }
+
+}
